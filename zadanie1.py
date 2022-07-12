dc6b006d7f168d1f3464aca710c564f69b9685a8
wi3rzbicki <90384318+wi3rzbicki@users.noreply.github.com>
2022-07-12 15:56:54 +0200

    Add files via upload

---
+++ zadanie1.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from requests import get
+
+URL = "https://www.polskawliczbach.pl/Miasta"
+page = get(URL)
+bs = BeautifulSoup(page.content, 'html.parser')
+
+for line in bs.find_all('td'):
+    city = line.find('a')
+
+URL2 = "https://www.polskawliczbach.pl/Wsie"
+page2 = get(URL2)
+bs2 = BeautifulSoup(page2.content, 'html.parser')
+
+for line2 in bs.find_all('td'):
+    village = line2.find('a')
