b455d4c68b04303f20d9a1ffba9e5ca013a25350
wi3rzbicki <90384318+wi3rzbicki@users.noreply.github.com>
2022-07-14 11:43:02 +0200

    Update zadanie1.py

--- zadanie1.py
+++ zadanie1.py
@@ -6,11 +6,11 @@ page = get(URL)
 bs = BeautifulSoup(page.content, 'html.parser')
 
 for line in bs.find_all('td'):
-    city = line.find('a')
+    city = line.find('a', href=True)
 
 URL2 = "https://www.polskawliczbach.pl/Wsie"
 page2 = get(URL2)
 bs2 = BeautifulSoup(page2.content, 'html.parser')
 
 for line2 in bs.find_all('td'):
-    village = line2.find('a')
+    village = line2.find('a', href=True)
