81147716bfd4e0206636e6c012d6f4ff07294cc1
wi3rzbicki <90384318+wi3rzbicki@users.noreply.github.com>
2022-07-29 14:43:32 +0200

    Update zadanie1.py

--- zadanie1.py
+++ zadanie1.py
@@ -1,16 +1,34 @@
 from bs4 import BeautifulSoup
 from requests import get
+import csv
 
-URL = "https://www.polskawliczbach.pl/Miasta"
-page = get(URL)
+URLC = "https://www.polskawliczbach.pl/Miasta"
+page = get(URLC)
 bs = BeautifulSoup(page.content, 'html.parser')
 
 for line in bs.find_all('td'):
     city = line.find('a', href=True)
 
-URL2 = "https://www.polskawliczbach.pl/Wsie"
-page2 = get(URL2)
+for x in city:
+    tab[x]=city
+    x += 1
+    #URLx = "https://www.polskawliczbach.pl/", tab[x]
+
+#data=
+
+URLW = "https://www.polskawliczbach.pl/Wsie"
+page2 = get(URLW)
 bs2 = BeautifulSoup(page2.content, 'html.parser')
 
 for line2 in bs.find_all('td'):
     village = line2.find('a', href=True)
+
+import csv
+
+f = open('path/to/csv_file', 'w', encoding='UTF8', newline='')
+
+writer = csv.writer(f)
+
+#writer.writerow(data)
+
+f.close()
