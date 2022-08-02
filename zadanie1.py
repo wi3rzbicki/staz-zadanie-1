3f362b73f62e7659bc11f8b987b86da10f3db29e
wi3rzbicki <90384318+wi3rzbicki@users.noreply.github.com>
2022-08-02 15:00:55 +0200

    Update zadanie1.py

--- zadanie1.py
+++ zadanie1.py
@@ -1,29 +1,58 @@
 from bs4 import BeautifulSoup
-from requests import get
+import requests
 import csv
+import json
+from tqdm import tqdm
 
 URLC = "https://www.polskawliczbach.pl/Miasta"
 page = get(URLC)
 bs = BeautifulSoup(page.content, 'html.parser')
+table_rows = soup.find("tbody").find_all("tr")
 
-for line in bs.find_all('td'):
-    city = line.find('a', href=True)
+for row in tqdm(rows, desc="Scraping cities"):
+    
+    #miasta
 
-for x in city:
-    tab[x]=city
-    x += 1
-    #URLx = "https://www.polskawliczbach.pl/", tab[x]
+    columns = row.find_all("td")
+    _miasto = {}
+    _miasto["name"] = columns[1].a.text
+
+    #polozenie
+
+    polozenie = resp.find("div", {"id": "polozenie"})
+    polozenie = polozenie.find_all("div")[1]
+    polozenie = polozenie.li.find_all("span")
+    _miasto["coordinates"] = {
+        "lat": float(polozenie[0].text), 
+        "lon": float(polozenie[1].text)
+        }
+
+    for li in powierzchnia:
+        if "Powierzchnia" in li.text:
+            _miasto["area"] = float(li.span.text[:-4].replace(",",".").replace(" ",""))
+
+    del _miasto["nazwa_uri"]
+
+    miasta.append(_miasto)
+
+with open ("text.json","w",encoding="utf-8") as f:
+    f.write(json.dumps(miasta, ensure_ascii=False))
+
+#for x in city:
+#    x = city
+#    tab = []
+#    tab.append(x)
+#URL = "https://www.polskawliczbach.pl/", city
 
 #data=
 
 URLW = "https://www.polskawliczbach.pl/Wsie"
 page2 = get(URLW)
 bs2 = BeautifulSoup(page2.content, 'html.parser')
+table_rows2 = soup.find("tbody").find_all("tr")
 
-for line2 in bs.find_all('td'):
-    village = line2.find('a', href=True)
-
-import csv
+for row in table_rows2:
+    village = row.find_all("td")[1].text
 
 f = open('path/to/csv_file', 'w', encoding='UTF8', newline='')
 
