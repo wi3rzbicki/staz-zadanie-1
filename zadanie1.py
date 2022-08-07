8aecb7717258192df22e0187ea19b2ac6cf641a0
wi3rzbicki <90384318+wi3rzbicki@users.noreply.github.com>
2022-08-07 22:25:17 +0200

    Update zadanie1.py

--- zadanie1.py
+++ zadanie1.py
@@ -2,20 +2,21 @@ from bs4 import BeautifulSoup
 from requests import get
 import csv
 from tqdm import tqdm
+    
+URLC = "https://www.polskawliczbach.pl/Miasta"
+page = get(URLC)
+bs = BeautifulSoup(page.content, 'html.parser')
+rows = bs.tbody.find_all("tr")
+_city = {}
 
 def get_cities():
-    
-    URLC = "https://www.polskawliczbach.pl/Miasta"
-    page = get(URLC)
-    bs = BeautifulSoup(page.content, 'html.parser')
-    rows = bs.tbody.find_all("tr")
 
     for row in tqdm(rows, desc="Scraping cities"):
     
+        columns = row.find_all("td")
+    
         #miasta
 
-        columns = row.find_all("td")
-        _city = {}
         _city["name"] = columns[1].a.text
         _city["name"] = _city["name"].replace(' ', '_').replace('-', '_').replace('Ó', 'O').replace('ó', 'o').replace('Ę', 'E').replace('ę', 'e').replace('Ą', 'A').replace('ą', 'a').replace('Ś', 'S').replace('ś', 's').replace('Ł', 'L').replace('ł', 'l').replace('Ż', 'Z').replace('ż', 'z').replace('Ź', 'Z').replace('ź', 'z').replace('Ć', 'C').replace('ć', 'c').replace('Ń', 'N').replace('ń', 'n')
 
@@ -23,23 +24,34 @@ def get_cities():
 
 get_cities()
 
-#
-    #_city["name_uri"] =  columns[1].a["href"]
+def get_geo():
+    
+    for row in tqdm(rows, desc="Scraping cities"):
 
-    #resp = get(_city["name_uri"])
-    #resp = BeautifulSoup(resp.text, "html.parser")
+        columns = row.find_all("td")
 
-    #polozenie
+        _city["name"] = columns[1].a.text
+        _city["name"] = _city["name"].replace(' ', '_').replace('-', '_').replace('Ó', 'O').replace('ó', 'o').replace('Ę', 'E').replace('ę', 'e').replace('Ą', 'A').replace('ą', 'a').replace('Ś', 'S').replace('ś', 's').replace('Ł', 'L').replace('ł', 'l').replace('Ż', 'Z').replace('ż', 'z').replace('Ź', 'Z').replace('ź', 'z').replace('Ć', 'C').replace('ć', 'c').replace('Ń', 'N').replace('ń', 'n')
 
-    #polozenie = resp.find("div", {"id": "polozenie"})
-    #polozenie = polozenie.find_all("div")[1]
-    #polozenie = polozenie.li.find_all("span")
-    #_city["coordinates"] = {
-    #    "lat": float(polozenie[0].text), 
-    #    "lon": float(polozenie[1].text)
-    #    }
 
-    #del _city["name_uri"]
+        #_city["name_uri"] = "https://www.polskawliczbach.pl/"_city["name"]""
+
+        resp = get(_city["name_uri"])
+        resp = BeautifulSoup(resp.text, "html.parser")
+
+        #polozenie
+
+        geo = resp.find("div", {"id": "geo"})
+        geo = geo.find_all("div")[1]
+        geo = geo.li.find_all("span")
+        _city["coordinates"] = {
+            "lat": float(geo[0].text), 
+            "lon": float(geo[1].text)
+            }
+
+#get_geo()
+
+#del _city["name_uri"]
 
     #cities.append(_city)
 
